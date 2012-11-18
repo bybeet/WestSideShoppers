e408555fbdc8448bcba628303bc9674c7d2f2341
Travis Bybee <travis.bybee@gmail.com>
2012-11-18 11:49:05 -0700

    Added in a basic layout file for the Recipes, using a list view. Changed the main screen to have a button for launching recipes for testing purposes.

--- WSS/src/com/csci422/westsideshoppers/WestSideShoppers.java
+++ WSS/src/com/csci422/westsideshoppers/WestSideShoppers.java
@@ -1,15 +1,31 @@
 package com.csci422.westsideshoppers;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class WestSideShoppers extends Activity {
 
+	
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_west_side_shoppers);
+        
+        Button btn = (Button)findViewById(R.id.buttonRecipes);
+        btn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(WestSideShoppers.this, com.csci422.westsideshoppers.Recipes.class);
+				startActivity(i);
+			}
+		});
+        
     }
 
     @Override
