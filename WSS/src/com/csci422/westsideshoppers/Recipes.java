171f20677cf9e7d888a35c852fcaa10d0da6566e
Travis Bybee <travis.bybee@gmail.com>
2012-11-18 12:18:43 -0700

    Added in a row layout that each individual recipe will use. Added a RecipeAdapter and a RecipeHolder in recipe to populate the list from the database and modify the row views.

--- WSS/src/com/csci422/westsideshoppers/Recipes.java
+++ WSS/src/com/csci422/westsideshoppers/Recipes.java
@@ -1,14 +1,74 @@
 package com.csci422.westsideshoppers;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
 
-public class Recipes extends Activity {
+public class Recipes extends ListActivity {
+	
+	private Cursor recipes;
+	private RecipeHelper helper;
+	private RecipeAdapter adapter;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceBundle){
 		super.onCreate(savedInstanceBundle);
 		setContentView(R.layout.activity_recipes);
+		
+		initRecipeList();
 	}
+	
+	private void initRecipeList(){
+		if(recipes != null){
+			stopManagingCursor(recipes);
+			recipes.close();
+		}
+		
+		recipes = helper.getAll("name");
+		startManagingCursor(recipes);
+		
+		adapter = new RecipeAdapter(recipes);
+		setListAdapter(adapter);
+	}
+	
+	class RecipeAdapter extends CursorAdapter {
+		RecipeAdapter(Cursor c){
+			super(Recipes.this, c);
+		}
+		
+		@Override
+		public void bindView(View row, Context ctxt, Cursor c) {
+			RecipeHolder holder = (RecipeHolder)row.getTag();
+			holder.populateFrom(c, helper);
+		}
+		
+		@Override
+		public View newView(Context ctxt, Cursor c, ViewGroup parent){
+			LayoutInflater inflater = getLayoutInflater();
+			View row = inflater.inflate(R.layout.row, parent, false);
+			RecipeHolder holder = new RecipeHolder(row);
+			row.setTag(holder);
+			return row;
+		}
+	}
+	
+	static class RecipeHolder {
+		private TextView name;
+		
+		RecipeHolder (View row){
+			name = (TextView)row.findViewById(R.id.recipeName);
+		}
+		
+		void populateFrom(Cursor c, RecipeHelper helper){
+			name.setText(helper.getName(c));
+		}
+	}
+	
 	
 }
