9d9960fdb55afb6e3f963395cb86453a17a1b6f7
Travis Bybee <travis.bybee@gmail.com>
2012-11-18 11:58:14 -0700

    Changed database class to work for Recipes.

--- WSS/src/com/csci422/westsideshoppers/RecipeHelper.java
+++ WSS/src/com/csci422/westsideshoppers/RecipeHelper.java
@@ -2,15 +2,13 @@ package com.csci422.westsideshoppers;
 
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.preference.PreferenceManager;
 
 class RecipeHelper extends SQLiteOpenHelper {
-	private static final String DATABASE_NAME = "lunchlist.db";
-	private static final int SCHEMA_VERSION = 3;
+	private static final String DATABASE_NAME = "recipes.db";
+	private static final int SCHEMA_VERSION = 1;
 
 
 	public RecipeHelper(Context context) {
@@ -20,90 +18,43 @@ class RecipeHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 
-		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT, feed TEXT, lat REAL, lon REAL);");
-	}
-
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		if(oldVersion < 2)
-			db.execSQL("ALTER TABLE restaurants ADD COLUMN feed TEXT");
-		if(oldVersion < 3) {
-			db.execSQL("ALTER TABLE restaurants ADD COLUMN lat REAL");
-			db.execSQL("ALTER TABLE restaurants ADD COLUMN lon REAL");
-		}
-			
+		db.execSQL("CREATE TABLE recipes (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);");
 	}
 
 	public Cursor getAll(String orderBy) {
-		return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, feed, lat, lon FROM restaurants ORDER BY "+orderBy, null);
+		return getReadableDatabase().rawQuery("SELECT _id, name FROM recipes ORDER BY "+orderBy, null);
 	}
 
 	public Cursor getById(String id) {
 		String[] args = {id};
 
-		return getReadableDatabase().rawQuery("SELECT _id, name, address, type, notes, feed, lat, lon FROM restaurants WHERE _ID=?", args);
+		return getReadableDatabase().rawQuery("SELECT _id, name FROM recipes WHERE _ID=?", args);
 	}
 
-	public void insert(String name, String address, String type, String notes, String feed) {
+	public void insert(String name) {
 		ContentValues cv=new ContentValues();
 
 		cv.put("name", name);
-		cv.put("address", address);
-		cv.put("type", type);
-		cv.put("notes", notes);
-		cv.put("feed", feed);
 
-		getWritableDatabase().insert("restaurants", "name", cv);
+		getWritableDatabase().insert("recipes", "name", cv);
 	}
 
-	public void update(String id, String name, String address, String type, String notes, String feed) {
+	public void update(String id, String name) {
 		ContentValues cv=new ContentValues();
 		String[] args={id};
 
 		cv.put("name", name);
-		cv.put("address", address);
-		cv.put("type", type);
-		cv.put("notes", notes);
-		cv.put("feed", feed);
 
-		getWritableDatabase().update("restaurants", cv, "_ID=?", args);
-	}
-	
-	public void updateLocation(String id, double lat, double lon) {
-		ContentValues cv = new ContentValues();
-		String[] args = {id};
-		
-		cv.put("lat", lat);
-		cv.put("lon", lon);
-		
-		getWritableDatabase().update("restaurants", cv, "_ID=?", args);
+		getWritableDatabase().update("recipes", cv, "_ID=?", args);
 	}
 
 	public String getName(Cursor c) {
 		return c.getString(1);
 	}
 
-	public String getAddress(Cursor c) {
-		return c.getString(2);
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		//Nothing to do here, on db scheme 1
 	}
 
-	public String getType(Cursor c) {
-		return c.getString(3);
-	}
-
-	public String getNotes(Cursor c) {
-		return c.getString(4);
-	}
-	
-	public String getFeed(Cursor c) {
-		return c.getString(5);
-	}
-	
-	public double getLatitude(Cursor c) {
-		return c.getDouble(6);
-	}
-	
-	public double getLongitude(Cursor c) {
-		return c.getDouble(7);
-	}
 }
