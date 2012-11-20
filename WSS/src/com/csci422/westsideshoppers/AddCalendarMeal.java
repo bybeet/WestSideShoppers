cfed78851893c22a69c9bc3aa41e42a574f10136
Travis Bybee <travis.bybee@gmail.com>
2012-11-20 03:15:23 -0700

    Added basic button functionality. Spinner selection is now stored in a local string variable.

--- WSS/src/com/csci422/westsideshoppers/AddCalendarMeal.java
+++ WSS/src/com/csci422/westsideshoppers/AddCalendarMeal.java
@@ -4,7 +4,14 @@ import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AddCalendarMeal extends Activity{
 	
@@ -16,16 +23,36 @@ public class AddCalendarMeal extends Activity{
 	
 	private Spinner spinner;
 	
+	private boolean somethingSelected;
+	private String recipeName;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_calendar_meal);
+		
+		somethingSelected = false;
 		
 		recipeHelper = new RecipeHelper(this);
 		calHelper = new  CalendarHelper(this);
 		spinner = (Spinner)findViewById(R.id.spinner);
 		
 		populateSpinner();
+		
+		Button btn = (Button)findViewById(R.id.addMealCal);
+		btn.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				if(!somethingSelected){				
+					Toast.makeText(AddCalendarMeal.this, "No meal selected.", Toast.LENGTH_SHORT).show();
+				}
+				else{
+					finish();
+				}
+			}
+			
+		});
 	}
 	
 	@Override
@@ -50,5 +77,21 @@ public class AddCalendarMeal extends Activity{
 		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.row, recipes, columns, to);
 		adapter.setDropDownViewResource(R.layout.row);
 		spinner.setAdapter(adapter);
+		
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+			
+			public void onNothingSelected(AdapterView<?> parent){
+				
+			}
+
+			@Override
+			public void onItemSelected(AdapterView<?> arg0, View arg1, int pos, long id) {
+				somethingSelected = true;
+				recipes = (Cursor)spinner.getItemAtPosition(pos);
+				recipeName = recipeHelper.getName(recipes);
+				Log.v("Spinner", recipeHelper.getName(recipes));
+			}
+		
+		});
 	}
 }
