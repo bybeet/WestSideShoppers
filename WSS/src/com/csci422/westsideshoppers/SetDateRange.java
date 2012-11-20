e7cda6677fe559e2e2a245c08297e0ff27ae3490
Travis Bybee <travis.bybee@gmail.com>
2012-11-20 04:15:42 -0700

    Added beginning functionality to Shopping activity. Created SetDateRange activity and create new layout. Fixed name issue with rowCheckBox layout.

---
+++ WSS/src/com/csci422/westsideshoppers/SetDateRange.java
@@ -0,0 +1,13 @@
+package com.csci422.westsideshoppers;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SetDateRange extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_set_date_range);
+	}
+}
--- WSS/src/com/csci422/westsideshoppers/Shopping.java
+++ WSS/src/com/csci422/westsideshoppers/Shopping.java
@@ -1,13 +1,42 @@
 package com.csci422.westsideshoppers;
 
+import java.util.Date;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Shopping extends Activity {
+	
+	private Cursor recipe;
+	private Cursor calendar;
+	
+	private RecipeHelper recipeHelper;
+	private CalendarHelper calHelper;
+	
+	private Date beginDate;
+	private Date endDate;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceBundle){
 		super.onCreate(savedInstanceBundle);
 		setContentView(R.layout.activity_shopping);
+		
+		Button btn = (Button)findViewById(R.id.datePicker);
+		btn.setOnClickListener(new OnClickListener(){
+		
+			@Override
+			public void onClick(View view){
+				Intent i = new Intent(Shopping.this, SetDateRange.class);
+				startActivity(i);
+			}
+			
+		});
+		
+		
 	}
 }
