60e854a8274a18060daf784cf4dd1b3c744e2991
Travis Bybee <travis.bybee@gmail.com>
2012-11-20 02:09:43 -0700

    Added activity for adding meals to calendar. Basic layout and changed manifest.

---
+++ WSS/src/com/csci422/westsideshoppers/AddCalendarMeal.java
@@ -0,0 +1,10 @@
+package com.csci422.westsideshoppers;
+
+public class AddCalendarMeal extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_calendar_meal);
+	}
+}
