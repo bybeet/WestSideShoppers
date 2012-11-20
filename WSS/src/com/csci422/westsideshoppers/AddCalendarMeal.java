d70a3c35e10353ba62bb00184392a12f7520eeb9
Travis Bybee <travis.bybee@gmail.com>
2012-11-20 02:32:33 -0700

    Create add a meal to the calendar layout.

--- WSS/src/com/csci422/westsideshoppers/AddCalendarMeal.java
+++ WSS/src/com/csci422/westsideshoppers/AddCalendarMeal.java
@@ -1,13 +1,25 @@
 package com.csci422.westsideshoppers;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.DialogFragment;
+import android.database.Cursor;
 import android.os.Bundle;
 
 public class AddCalendarMeal extends Activity{
+	
+	private Cursor recipeCursor;
+	private Cursor calCursor;
+	
+	private RecipeHelper recipeHelper;
+	private CalendarHelper calHelper;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_calendar_meal);
+		
+		
 	}
+	
 }
---
+++ WSS/src/com/csci422/westsideshoppers/CalendarHelper.java
@@ -0,0 +1,64 @@
+package com.csci422.westsideshoppers;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CalendarHelper extends SQLiteOpenHelper {
+	private static final String DATABASE_NAME = "calendar.db";
+	private static final int SCHEMA_VERSION = 1;
+
+
+	public CalendarHelper(Context context) {
+		super(context, DATABASE_NAME, null, SCHEMA_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE calendar (_id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, recipe TEXT);");
+	}
+
+	public Cursor getAll() {
+		return getReadableDatabase().rawQuery("SELECT _id, date, recipe FROM recipes", null);
+	}
+
+	public Cursor getById(String id) {
+		String[] args = {id};
+
+		return getReadableDatabase().rawQuery("SELECT _id, date, recipe FROM recipes WHERE _ID=?", args);
+	}
+
+	public void insert(String date, String recipe){
+		ContentValues cv=new ContentValues();
+
+		cv.put("date", date);
+		cv.put("recipe", recipe);
+
+		getWritableDatabase().insert("calendar", "date", cv);
+	}
+
+	public void update(String id, String date, String recipe){
+		ContentValues cv=new ContentValues();
+		String[] args={id};
+
+		cv.put("date", date);
+		cv.put("recipe", recipe);
+		
+		getWritableDatabase().update("calendar", cv, "_ID=?", args);
+	}
+
+	public String getDate(Cursor c) {
+		return c.getString(1);
+	}
+	
+	public String getRecipe(Cursor c){
+		return c.getString(2);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		//Nothing to do here, on db scheme 1
+	}
+}
